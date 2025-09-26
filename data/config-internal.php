b22e4701d5f71dffdf54f518cca5f4b73f34cad2
DarthPaul2 <malanov2@gmail.com>
2025-09-26 12:53:34 +0000

    Neues Feature: Mahnwesen + Fixes

---
+++ data/config-internal.php
@@ -0,0 +1,38 @@
+<?php
+return [
+  'actualDatabaseType' => 'mysql',
+  'actualDatabaseVersion' => '8.0.43',
+  'microtimeInternal' => 1756904986.803182,
+  'instanceId' => 'ab6f82be-80a7-4ab3-98dd-fe0eef820244',
+  'smtpPassword' => NULL,
+  'logger' => [
+    'path' => 'data/logs/espo.log',
+    'level' => 'WARNING',
+    'rotation' => true,
+    'maxFileNumber' => 30,
+    'printTrace' => false,
+    'databaseHandler' => false,
+    'sql' => false,
+    'sqlFailed' => false
+  ],
+  'restrictedMode' => false,
+  'cleanupAppLog' => true,
+  'cleanupAppLogPeriod' => '30 days',
+  'webSocketMessager' => 'ZeroMQ',
+  'clientSecurityHeadersDisabled' => false,
+  'clientCspDisabled' => false,
+  'clientCspScriptSourceList' => [
+    0 => 'https://maps.googleapis.com'
+  ],
+  'adminUpgradeDisabled' => false,
+  'database' => [
+    'host' => 'localhost',
+    'port' => 3306,
+    'charset' => 'utf8mb4',
+    'dbname' => 'espocrm',
+    'user' => 'pavel',
+    'password' => 'klesec.481263!!',
+    'driver' => 'pdo_mysql'
+  ],
+  'isInstalled' => true
+];
